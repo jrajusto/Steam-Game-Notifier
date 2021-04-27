35fd13b7af8cdb0d03115a73e45d6a34e696261b
jrajusto <jrajusto@mymail.mapua.edu.ph>
2021-04-27 17:37:06 +0800

    code for adding to database

---
+++ Steam_games.py
@@ -0,0 +1,53 @@
+import sqlite3
+import requests
+import json
+
+dealAPI = '44587204c43f4187a3b2b01f590949494717aa14'
+steamKey = 'A6434537190388EB7A022C0C6780946B'
+steamAPI = 'http://api.steampowered.com/<interface name>/<method name>/v<version>/?key=<' + steamKey + '>&format=<json>'
+response = requests.get('https://api.steampowered.com/ISteamApps/GetAppList/v2/?key=A6434537190388EB7A022C0C6780946B')
+key = response.json()
+site = "https://api.isthereanydeal.com/v01/game/prices/?key="+dealAPI+"&plains="
+
+response2 =  requests.get("https://api.isthereanydeal.com/v01/game/plain/list/?key=" + dealAPI + "&shops=steam")
+key2 = response2.json()
+tempList = []
+
+for i in key['applist']['apps']:
+    plains =  key2['data']['steam'].get('app/'+str(i['appid']))
+    if plains != None:
+        temp = (i['appid'],i['name'],plains)
+        tempList.append(temp)
+    
+counter = 0
+site2 = site
+counter2 = 0
+max = len(tempList)
+gameList = []
+finalList = []
+for i in tempList:
+    counter2 = counter2 + 1
+    counter = counter + 1
+    plains = i[2]
+    site2 = site2 + plains +"%2C"
+    gameList.append(i)
+    if counter == 150 or counter2 == max:
+        counter = 0
+        response = requests.get(site2+"&region=us&country=US")
+        site2 = site
+        key = response.json()
+        for j in gameList:
+            shopList = key['data'][j[2]].get("list")
+            if len(shopList)!=0:
+                finalList.append(j)
+                print(finalList)
+        
+        gameList = []
+
+conn = sqlite3.connect('Steam Games.db')
+c = conn.cursor()
+c.executemany("INSERT INTO Steam_games VALUES (?,?,?)",finalList)
+
+ 
+conn.commit()
+conn.close()
