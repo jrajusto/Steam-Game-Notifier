27eddec9b53c3396cf7b7288eb7702069a261c50
jrajusto <jrajusto@mymail.mapua.edu.ph>
2021-04-27 19:03:05 +0800

    add functions for database manipulation

---
+++ sqlite_database.py
@@ -0,0 +1,37 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    conn = None
+    try: 
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def update_Store(conn, data):
+    
+    sql = ''' UPDATE Store
+              SET current price = ? ,
+                  discount = ? ,
+              WHERE id = ?'''
+    cur = conn.cursor()
+    cur.execute(sql, data)
+    conn.commit()
+
+def getStoreID(conn, appID):
+    sql = ''' SELECT storeID WHERE id = ?'''
+
+    cur = conn.cursor()
+    cur.execute(sql, appID)
+
+    return cur.fetchone()
+
+def getAppID(conn, storeID):
+    sql = ''' SELECT appID WHERE id = ?'''
+
+    cur = conn.cursor()
+    cur.execute(sql, storeID)
+    
+    return cur.fetchone()
